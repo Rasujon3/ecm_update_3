1d990e1982c2d5b76a3bd0cb61422af5cfd18199
Sujon Office <>
2025-08-31 11:07:29 +0600

    timer done

--- app/Http/Controllers/TimerController.php
+++ app/Http/Controllers/TimerController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\TimerRequest;
+use App\Models\ModuleTutorial;
 use App\Models\Timer;
 use Carbon\Carbon;
 use Exception;
@@ -17,13 +18,65 @@ class TimerController extends Controller
     }
     public function index()
     {
-        $data = Timer::where('user_id', user()->id)->first();
-        return view('timer.timer',compact('data'));
+        $selection = getCurrentSelection();
+        $domainId = $selection['domain_id'];
+        $subDomainId = $selection['sub_domain_id'];
+
+        if ((!$domainId && !$subDomainId)) {
+            $notification=array(
+                'messege' => 'Domain & Subdomain mismatch.',
+                'alert-type' => 'error'
+            );
+            return redirect()->back()->with($notification);
+        }
+
+        $moduleName = 'Timer';
+        $url = null;
+        $tutorial = null;
+        if (!empty($moduleName)) {
+            $tutorial = ModuleTutorial::where('module_title', trim($moduleName))->first();
+        }
+        if($tutorial && !empty($tutorial->video_url)) {
+            $url = $this->getYoutubeEmbedUrl($tutorial->video_url);
+        }
+
+        $data = Timer::where('user_id', user()->id)
+            ->where('domain_id', $domainId)
+            ->where('sub_domain_id', $subDomainId)
+            ->first();
+        return view('timer.timer',compact('data', 'url'));
+    }
+    function getYoutubeEmbedUrl($url)
+    {
+        $pattern_long = '/(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=))([^"&?\/ ]{11})/';
+        $pattern_short = '/youtu\.be\/([^"&?\/ ]{11})/';
+
+        if (preg_match($pattern_long, $url, $matches)) {
+            $videoId = $matches[1];
+        } elseif (preg_match($pattern_short, $url, $matches)) {
+            $videoId = $matches[1];
+        } else {
+            return null; // Not a valid YouTube URL
+        }
+
+        return 'https://www.youtube.com/embed/' . $videoId;
     }
     public function store(TimerRequest $request)
     {
         try
         {
+            $selection = getCurrentSelection();
+            $domainId = $selection['domain_id'];
+            $subDomainId = $selection['sub_domain_id'];
+
+            if ((!$domainId && !$subDomainId)) {
+                $notification=array(
+                    'messege' => 'Domain & Subdomain mismatch.',
+                    'alert-type' => 'error'
+                );
+                return redirect()->back()->with($notification);
+            }
+
             $data = Timer::where('user_id', user()->id)->first();
 
             $defaults = [
@@ -35,7 +88,8 @@ class TimerController extends Controller
                 Timer::where('id', $data->id)->update(
                     [
                         'user_id' => user()->id,
-			            'domain_id' => getDomain()->id,
+			            'domain_id' => $domainId,
+			            'sub_domain_id' => $subDomainId,
 //                        'title' => $request->title ?? $defaults['title'],
 //                        'time' => Carbon::parse($request->time)->timestamp ?? $defaults['time'],
                         'title' => $request->title ?? '',
@@ -46,7 +100,8 @@ class TimerController extends Controller
                 Timer::create(
                     [
                         'user_id' => user()->id,
-			            'domain_id' => getDomain()->id,
+                        'domain_id' => $domainId,
+                        'sub_domain_id' => $subDomainId,
 //                        'title' => $request->title ?? $defaults['title'],
 //                        'time' => Carbon::parse($request->time)->timestamp ?? $defaults['time'],
                         'title' => $request->title ?? '',
